a60d20ae9e525d73f9e54381e0cc3c7a690a5e71
Caique Souza Fonseca <c.souzaf@hotmail.com>
2023-01-08 12:16:56 -0300

    Update banco.js

--- Express/EstruturaBanco/models/banco.js
+++ Express/EstruturaBanco/models/banco.js
@@ -1,6 +1,6 @@
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('testenodejs', 'root', 'Unitedred33.', {
+const sequelize = new Sequelize('testenodejs', 'root', 'null', {
     host:'localhost',
     dialect: 'mysql'
 })
